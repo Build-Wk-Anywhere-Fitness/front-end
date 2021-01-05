94ca31dec535dd1568b53be8ef6b283822161cc0
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-01-05 12:02:01 -0700

    updated instructor form with inputs and submit btn

--- src/components/signup_forms/InstructorForm.js
+++ src/components/signup_forms/InstructorForm.js
@@ -20,8 +20,62 @@ export default function InstructorForm(props) {
         change(name, value)
     }
     return (
-        <div>
-
-        </div>
+        <form className='signup-section' onSubmit={onSubmit}>
+            <div className="signup-select-btn">
+                <h2>Sign Up!</h2>
+                <div className='errors'>
+                    {/* validation errors */}
+                    <div>{errors.username}</div>
+                    <div>{errors.email}</div>
+                    <div>{errors.role}</div>
+                </div>
+            </div>
+            <div className='main-section'>
+                <h4>General information</h4>
+                <label>
+                    Username
+                     <input
+                        value={values.username}
+                        name='username'
+                        onChange={onChange}
+                        type="text"
+                        placeholder="Username"
+                    />
+                </label>
+                <label>
+                    Password
+                     <input
+                        value={values.password}
+                        name='password'
+                        onChange={onChange}
+                        type="password"
+                        placeholder="Password"
+                    />
+                </label>
+                <label>
+                    Email
+                     <input
+                        value={values.email}
+                        name='email'
+                        onChange={onChange}
+                        type="email"
+                        placeholder="Email"
+                    />
+                </label>
+                <label>
+                    Role
+                     <select
+                        value={values.role}
+                        name='role'
+                        onChange={onChange}
+                    >
+                        <option value="">- Select an option -</option>
+                        <option value="user">User</option>
+                        <option value="instructor">Instructor</option>
+                    </select>
+                </label>
+                <button disabled={disabled}>submit</button>
+            </div>
+        </form>
     )
 }
