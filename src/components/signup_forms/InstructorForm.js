7766f68f0461cdb284e33d978b73ef9f78e9ceb6
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-01-05 11:06:27 -0700

    began instructor reg

--- src/components/signup_forms/InstructorForm.js
+++ src/components/signup_forms/InstructorForm.js
@@ -1,1 +1,10 @@
 // Create a form for users that wish to sign up as an instructor, should take in an additional value to flag the account as an instructor account
+import React from 'react'
+
+export default function InstructorForm() {
+    return (
+        <div>
+
+        </div>
+    )
+}
