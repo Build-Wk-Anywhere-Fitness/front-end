fa35eee7eed817a2751a8be47785e34372cec86c
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-01-05 11:26:43 -0700

    imported instructor form

--- src/components/signup_forms/InstructorForm.js
+++ src/components/signup_forms/InstructorForm.js
@@ -1,7 +1,24 @@
 // Create a form for users that wish to sign up as an instructor, should take in an additional value to flag the account as an instructor account
 import React from 'react'
 
-export default function InstructorForm() {
+export default function InstructorForm(props) {
+    const {
+        values,
+        submit,
+        change,
+        disabled,
+        errors,
+    } = props;
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
+
+    const onChange = evt => {
+        const { name, value } = evt.target
+        change(name, value)
+    }
     return (
         <div>
 
