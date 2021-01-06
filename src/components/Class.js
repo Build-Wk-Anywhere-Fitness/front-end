83437a91042cc91bfd6c4ee1c6b0bc48e0268af6
lljmb <brown.jessm@icloud.com>
2021-01-06 15:11:38 -0500

    added edit class page & created new landing page to add/edit classes

--- src/components/Class.js
+++ src/components/Class.js
@@ -1,24 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function User(props){
-    const { details } = props;
-
-    if(!details){
-        return <h3>working on fetching the class details</h3>
-    }
-
-    return ( 
-        <div className='container'>
-            <h2>Name: {details.name}</h2>
-            <h4>Special Instructions: {details.specialIns || 'n/a'}</h4>
-            <p>Location: {details.location}</p>
-            <p>Duration: {details.duration}</p>
-            <p>Intensity: {details.intensity}</p>
-            <p>Type of Workout: {details.boxing === true ? 'boxing' : ''}
-             {details.pilates === true ? ' pilates' : ''} 
-             {details.yoga === true ? ' yoga' : ''}
-             {details.hiit === true ? ' hiit' : ''}
-             {details.crossfit === true ? ' crossfit' : ''}</p>
-        </div>
+export default function ClassesDir(props) {
+    return (
+        <section className="signup-section">
+            <h1>Create or Edit Classes</h1>
+            <div>
+                <Link className="signup-select-btn" to="/create-class" >Create New</Link>
+                <Link className="signup-select-btn" to="/edit-class" >Edit Existing</Link>
+            </div>
+        </section>
     )
 }
